8be0eacfc097dd40e42a45375f717a70c4e7d8d3
PaulinaFranco <dadiante@gmail.com>
2019-10-14 15:27:03 -0500

    Updates

---
+++ precache-manifest.dd8b45a7dadf0133cb376bd5e78105ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf48dc283dc2179f3f19116bf912cbb",
+    "url": "/GDL003-Burger-Queen-Front-End/index.html"
+  },
+  {
+    "revision": "4aee64c9a49626c6eca7",
+    "url": "/GDL003-Burger-Queen-Front-End/static/css/main.63468f15.chunk.css"
+  },
+  {
+    "revision": "fb1b6472fff2e3fe6193",
+    "url": "/GDL003-Burger-Queen-Front-End/static/js/2.56432c77.chunk.js"
+  },
+  {
+    "revision": "4aee64c9a49626c6eca7",
+    "url": "/GDL003-Burger-Queen-Front-End/static/js/main.8ae1a8a9.chunk.js"
+  },
+  {
+    "revision": "ecfde5d930d662f6ef32",
+    "url": "/GDL003-Burger-Queen-Front-End/static/js/runtime-main.9136da15.js"
+  }
+]);
